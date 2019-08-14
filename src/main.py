4489f8efc01953d0e2486cbaf818e4e5c7877023
Julio Londono <juliolondonogarcia@gmail.com>
2019-08-14 00:12:13 +0000

    First commit with update files

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Person
+from models import db, User, Product, Address, BillingAddress, Picture
+
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -17,6 +21,37 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
+# /////////////////////////////////////// JWT configuration///////////////////////////////////////
+# Setup the Flask-JWT-Simple extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+
+# Provide a method to create access tokens. The create_jwt()
+# function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    userName = params.get('userName', None)
+    email = params.get('email', None)
+
+    if not userName:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not email:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    usercheck = User.query.filter_by(userName=userName, email=email).first()
+    if usercheck == None:
+        return jsonify({"msg": "Bad username or email"}), 401
+    # if username != 'test' or password != 'test':
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=userName)}
+    return jsonify(ret), 200
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -25,7 +60,9 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/person', methods=['POST', 'GET'])
+    # //////////////////////////// Create Person Endpoints //////////////////////////////////////////////////
+
+@app.route('/user', methods=['POST', 'GET'])
 def handle_person():
     """
     Create person and retrieve all persons
@@ -37,26 +74,29 @@ def handle_person():
 
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
-        if 'username' not in body:
+        if 'userName' not in body:
             raise APIException('You need to specify the username', status_code=400)
         if 'email' not in body:
             raise APIException('You need to specify the email', status_code=400)
+        if 'password' not in body:
+            raise APIException('You need to enter your password', status_code=400)
 
-        user1 = Person(username=body['username'], email=body['email'])
+        user1 = User(userFirstName=body['userFirstName'], userLastName=body['userLastName'], userName=body['userName'], email=body['email'], password=body['password'], addresses=body['addresses'])
         db.session.add(user1)
         db.session.commit()
         return "ok", 200
 
     # GET request
     if request.method == 'GET':
-        all_people = Person.query.all()
+        all_people = User.query.all()
         all_people = list(map(lambda x: x.serialize(), all_people))
         return jsonify(all_people), 200
 
     return "Invalid Method", 404
 
 
-@app.route('/person/<int:person_id>', methods=['PUT', 'GET', 'DELETE'])
+@app.route('/user/<int:person_id>', methods=['PUT', 'GET', 'DELETE'])
+@jwt_required #this decorator makes this requires to be logged in
 def get_single_person(person_id):
     """
     Single person
@@ -68,7 +108,7 @@ def get_single_person(person_id):
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
 
-        user1 = Person.query.get(person_id)
+        user1 = User.query.get(person_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
 
@@ -82,14 +122,14 @@ def get_single_person(person_id):
 
     # GET request
     if request.method == 'GET':
-        user1 = Person.query.get(person_id)
+        user1 = User.query.get(person_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
         return jsonify(user1.serialize()), 200
 
     # DELETE request
     if request.method == 'DELETE':
-        user1 = Person.query.get(person_id)
+        user1 = User.query.get(person_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
         db.session.delete(user1)
@@ -98,7 +138,322 @@ def get_single_person(person_id):
 
     return "Invalid Method", 404
 
+    # //////////////////////////////////////////////////// Create Products end points  /////////////////////////////////
 
+@app.route('/product', methods=['POST', 'GET'])
+def handle_product():
+    """
+    Create product and retrieve all products
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'productName' not in body:
+            raise APIException('You need to specify the product name', status_code=400)
+        if 'productPrice' not in body:
+            raise APIException('You need to specify the product price', status_code=400)
+
+        product1 = Product(productName=body['productName'], productDescription=body['productDescription'], productPrice=body['productPrice'], productCategory=body['productCategory'], productAgeRange=body['productAgeRange'])
+        db.session.add(product1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_products = Product.query.all()
+        all_products = list(map(lambda x: x.serialize(), all_products))
+        return jsonify(all_products), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/product/<int:product_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_product(product_id):
+    """
+    Single product
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        product1 = Product.query.get(product_id)
+        if product1 is None:
+            raise APIException('Product not found', status_code=404)
+
+        if "productName" in body:
+            product1.productName = body["productName"]
+        if "productPrice" in body:
+            product1.productPrice = body["productPrice"]
+        db.session.commit()
+
+        return jsonify(product1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        product1 = Product.query.get(product_id)
+        if product1 is None:
+            raise APIException('Product not found', status_code=404)
+        return jsonify(product1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        product1 = Product.query.get(product_id)
+        if product1 is None:
+            raise APIException('Product not found', status_code=404)
+        db.session.delete(product1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+# ////////////////////////////////// User Address end points ///////////////////////////////////
+@app.route('/address', methods=['POST', 'GET'])
+def handle_address():
+    """
+    Create address and retrieve all address
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'userStreet' not in body:
+            raise APIException('You need to specify the street', status_code=400)
+        if 'userNumber' not in body:
+            raise APIException('You need to specify the address number', status_code=400)
+        if 'userCity' not in body:
+            raise APIException('You need to specify the city', status_code=400)
+        if 'userState' not in body:
+            raise APIException('You need to specify the state', status_code=400)
+        if 'userZipCode' not in body:
+            raise APIException('You need to specify the Zip Code', status_code=400)
+
+        address1 = Address(userStreet=body['userStreet'], userNumber=body['userNumber'], userCity=body['userCity'], userState=body['userState'], userZipCode=body['userZipCode'], isBillingAddress=body['isBillingAddress'], person_id=body['person_id'])
+        db.session.add(address1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_address = Address.query.all()
+        all_address = list(map(lambda x: x.serialize(), all_address))
+        return jsonify(all_address), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/address/<int:address_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_address(address_id):
+    """
+    Single address
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        address1 = Address.query.get(address_id)
+        if address1 is None:
+            raise APIException('address not found', status_code=404)
+
+        if "userStreet" in body:
+            address1.userStreet = body["userStreet"]
+        if "userNumber" in body:
+            address1.userNumber = body["userNumber"]
+        if "userCity" in body:
+            address1.userCity = body["userCity"]
+        if "userState" in body:
+            address1.userState = body["userState"]
+        if "userZipCode" in body:
+            address1.userZipCode = body["userZipCode"]
+        db.session.commit()
+
+        return jsonify(address1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        address1 = Address.query.get(address_id)
+        if address1 is None:
+            raise APIException('Address not found', status_code=404)
+        return jsonify(address1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        address1 = Address.query.get(address_id)
+        if address1 is None:
+            raise APIException('address not found', status_code=404)
+        db.session.delete(address1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+# //////////////////////////////////////////////Billing Address end points creation //////////////////////////////
+@app.route('/billingaddress', methods=['POST', 'GET'])
+def handle_billingaddress():
+    """
+    Create billing address and retrieve all address
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'billingStreet' not in body:
+            raise APIException('You need to specify the billing street', status_code=400)
+        if 'billingNumber' not in body:
+            raise APIException('You need to specify the billing address number', status_code=400)
+        if 'billingCity' not in body:
+            raise APIException('You need to specify the billing city', status_code=400)
+        if 'billingState' not in body:
+            raise APIException('You need to specify the billing state', status_code=400)
+        if 'billingZipCode' not in body:
+            raise APIException('You need to specify the billing Zip Code', status_code=400)
+
+        address1 = BillingAddress(billingStreet=body['billingStreet'], billingNumber=body['billingNumber'], billingCity=body['billingCity'],  billingState=body['billingState'], billingZipCode=body['billingZipCode'], person_id=body['person_id'])
+        db.session.add(address1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_billingaddress = BillingAddress.query.all()
+        all_billingaddress = list(map(lambda x: x.serialize(), all_billingaddress))
+        return jsonify(all_billingaddress), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/billingaddress/<int:billingaddress_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_billingaddress(billingaddress_id):
+    """
+    Single billing address
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        address1 = BillingAddress.query.get(billingaddress_id)
+        if address1 is None:
+            raise APIException('address not found', status_code=404)
+
+        if "billingStreet" in body:
+            address1.userStreet = body["billingStreet"]
+        if "billingNumber" in body:
+            address1.userNumber = body["billingNumber"]
+        if "billingCity" in body:
+            address1.billingCity = body["billingCity"]
+        if "billingState" in body:
+            address1.billingState = body["billingState"]
+        if "billingZipCode" in body:
+            address1.billingZipCode = body["billingZipCode"]
+        db.session.commit()
+
+        return jsonify(address1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        address1 = BillingAddress.query.get(billingaddress_id)
+        if address1 is None:
+            raise APIException('Billing Address not found', status_code=404)
+        return jsonify(address1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        address1 = BillingAddress.query.get(billingaddress_id)
+        if address1 is None:
+            raise APIException('Billing Address not found', status_code=404)
+        db.session.delete(address1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+# //////////////////////////////////////////// Picture End Point ////////////////////////////////
+@app.route('/picture', methods=['POST', 'GET'])
+def handle_picture():
+    """
+    Create picture URL and retrieve all pictures
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'picture_url' not in body:
+            raise APIException('You need to specify the picture URL', status_code=400)
+
+        address1 = Picture(picture_url=body['picture_url'], photos_id=body['photos_id'])
+        db.session.add(address1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_pictures = Picture.query.all()
+        all_pictures = list(map(lambda x: x.serialize(), all_pictures))
+        return jsonify(all_pictures), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/picture/<int:picture_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_picture(picture_id):
+    """
+    Single picture
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        address1 = BillingAddress.query.get(picture_id)
+        if address1 is None:
+            raise APIException('picture not found', status_code=404)
+
+        db.session.commit()
+
+        return jsonify(address1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        address1 = BillingAddress.query.get(picture_id)
+        if address1 is None:
+            raise APIException('picture not found', status_code=404)
+        return jsonify(address1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        address1 = Picture.query.get(picture_id)
+        if address1 is None:
+            raise APIException('picture not found', status_code=404)
+        db.session.delete(address1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+# /////////////////////////////////////// Start Server //////////////////////////////
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT)
--- src/models.py
+++ src/models.py
@@ -2,16 +2,140 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Person(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    userFirstName = db.Column(db.String(45), nullable=False)
+    userLastName = db.Column(db.String(45), nullable=False)
+    userName = db.Column(db.String(45), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(45), nullable=False)
+    addresses = db.relationship('Address', backref='person', lazy=True)
+    bill_address = db.relationship('BillingAddress', backref='person', lazy=True)
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return '<Person %r>' % self.userName
+
+    def serialize(self):
+        # need to create a dictionary (array) to be able to serialize by looping into an array
+        addresses = []
+        for i in self.addresses:
+            # to display just the id uncomment the following line
+            # addresses.append(i.id)
+            #to display the entire object data
+            addresses.append(i.serialize())
+        billAddress = []
+        for j in self.bill_address:
+            billAddress.append(j.serialize())
+
+        return {
+            "user id": self.id,
+            "userFirstName": self.userFirstName,
+            "userLastName": self.userLastName,
+            "userName": self.userName,
+            "email": self.email,
+            "password": self.password,
+            "addresses": addresses,  #call the empty array that was looped before the return
+            "Bill Address": billAddress
+        }
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    productName = db.Column(db.String(45), unique=True, nullable=False)
+    productDescription = db.Column(db.String(255), unique=True, nullable=True)
+    productPrice = db.Column(db.String(45), unique=False, nullable=False)
+    productCategory = db.Column(db.String(45), unique=False, nullable=True)
+    productAgeRange = db.Column(db.String(45), unique=False, nullable=True)
+    pictureUrl = db.relationship('Picture', backref='photos', lazy=True)
+    # bill_address = db.relationship('BillingAddress', backref='person', lazy=True)
+
+    def __repr__(self):
+        return '<Product %r>' % self.productName
+
+    def serialize(self):
+        photo = []
+        for i in self.pictureUrl:
+            photo.append(i.serialize())
+        return {
+            "Product id": self.id,
+            "ProductName": self.productName,
+            "productDescription": self.productDescription,
+            "productPrice": self.productPrice,
+            "productCategory": self.productCategory,
+            "productAgeRange": self.productAgeRange,
+            "photo": photo
+        }
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userStreet = db.Column(db.String(45), nullable=False)
+    userNumber = db.Column(db.String(45), nullable=False)
+    userCity = db.Column(db.String(45), nullable=False)
+    userState = db.Column(db.String(45), nullable=False)
+    userZipCode = db.Column(db.String(12), nullable=False)
+    isBillingAddress=db.Column(db.Boolean)
+    person_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+
+
+    def __repr__(self):
+        return '<Addres %r>' % self.userStreet
 
     def serialize(self):
         return {
-            "username": self.username,
-            "email": self.email
+            "street id": self.id,
+            "User Street": self.userStreet,
+            "User Number": self.userNumber,
+            "User City": self.userCity,
+            "User State": self.userState,
+            "User Zip Code": self.userZipCode,
+            "Same as Billing Address": self.isBillingAddress,
+            "user": self.person_id
+        }
+
+class BillingAddress(db.Model):
+    __tablename__ = 'billing_addresses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    billingStreet = db.Column(db.String(45), nullable=True)
+    billingNumber = db.Column(db.String(45), nullable=True)
+    billingCity = db.Column(db.String(45), nullable=True)
+    billingState = db.Column(db.String(45), nullable=True)
+    billingZipCode = db.Column(db.String(12), nullable=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+
+    def __repr__(self):
+        return '<BillingAddress %r>' % self.billingStreet
+
+    def serialize(self):
+        return {
+            "Billing Street": self.billingStreet,
+            "Billing Number": self.billingNumber,
+            "Billing City": self.billingCity,
+            "Billing State": self.billingState,
+            "Billing Zip Code": self.billingZipCode,
+            "user": self.person_id
+
+        }
+
+class Picture(db.Model):
+    __tablename__ = 'pictures'
+
+    id = db.Column(db.Integer, primary_key=True)
+    picture_url = db.Column(db.Text, nullable=False)
+    photos_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    # person_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    def __repr__(self):
+        return '<Picture %r>' % self.picture_url
+
+    def serialize(self):
+        return {
+            "Picture URL": self.picture_url,
+            "product Id": self.photos_id
         }
